70438a16f34c89f0346b6656fe0a5566081f583d
ShaharLutatiPersonal <shahar761@gmail.com>
2020-04-25 07:52:02 +0200

    cleanup and ex2

---
+++ ex_2/src/RNN_models.py
@@ -0,0 +1,72 @@
+import torch.nn as nn
+import torch.autograd as grad
+import torch
+"""
+According to the article they defined an LSTM block as the following
+
+[i;f;o;g] = [sigm;sigm;sigm;tanh] * T_2n,4n * [h_t^l-1;h_t-1^l]
+where t - is subscript of time (first word is in time t = 0)
+and l is upscript of layer index.
+the LSTM cell defined as two main components
+c_t^l = f (*) c_t-1^l + i(*)g 
+h_t^l = o(*)tanh(c_t^l)
+where (*) is the operator for elementwise multiplication (Hadamard product)
+we can see that it is a kind of ARMA filter of the state
+where the moving average is the f(*)c part and the AR part is the i(*)g part
+with the use of tanh as the non linearity. As the coefficents of f is getting smaller
+the LSTM cell will exponenitally remember less in long term.
+
+In the article they introduced a new technique for dropout
+[i;f;o;g] = [sigm;sigm;sigm;tanh] * T_2n,4n * [D(h_t^l-1);h_t-1^l]
+meaning they doing a dropout on the inputs for the next layer.
+"""
+class RNN_Zam(nn.Module):
+    def __init__(self,dict_size,dp_prob = 0.0):
+        super(RNN_Zam, self).__init__()
+        self.embedding = nn.Embedding(num_embeddings = dict_size,embedding_dim = 200)
+        self.dropout = nn.Dropout(dp_prob)
+        # According to pytorch documentaion when applying a dropout !=0 it applies a dropout on the output except for the last layer.
+        # It does not apply the dropout to the state variable
+        # We double checked it on the source code of pytorch :)  (Google search: pytorch LSTM source -> _VF.lstm())
+        self.lstm = nn.LSTM(input_size = 200,hidden_size = 200 ,num_layers =  2,dropout = dp_prob)
+        self.fc = nn.Linear(in_features= 200,out_features= dict_size)
+        # According to the paper we should use weights initialization
+        # Reading Xavier weight initialization paper for symmetric non linearty (if we used relu we use Kaimen)
+        # we normalize by using the factor sqrt(6)/sqrt(num_input + num_output)
+        for parameter in self.parameters():
+            nn.init.uniform_(parameter,-.1,.1)
+        nn.init.xavier_uniform_(self.fc.weight)
+        nn.init.xavier_uniform_(self.embedding.weight)
+    def forward(self, input,inner_states):
+        output = self.embedding(input)
+        output = self.dropout(output)
+        output,(h_t,c) = self.lstm(output,inner_states)
+        output = self.dropout(output)
+        output = output.view(-1,200) 
+        output = self.fc(output)
+        return output,(h_t,c)
+class GRU(nn.Module):
+    def __init__(self,dict_size,dp_prob = 0.0):
+        super(GRU, self).__init__()
+        self.embedding = nn.Embedding(num_embeddings = dict_size,embedding_dim = 200)
+        self.dropout = nn.Dropout(dp_prob)
+        # According to pytorch documentaion when applying a dropout !=0 it applies a dropout on the output except for the last layer.
+        # It does not apply the dropout to the state variable
+        # We double checked it on the source code of pytorch :)  (Google search: pytorch LSTM source -> _VF.lstm())
+        self.gru = nn.GRU(input_size = 200,hidden_size = 200 ,num_layers =  2,dropout = dp_prob)
+        self.fc = nn.Linear(in_features= 200,out_features= dict_size)
+        # According to the paper we should use weights initialization
+        # Reading Xavier weight initialization paper for symmetric non linearty (if we used relu we use Kaimen)
+        # we normalize by using the factor sqrt(6)/sqrt(num_input + num_output)
+        for parameter in self.parameters():
+            nn.init.uniform_(parameter,-.1,.1)
+        nn.init.xavier_uniform_(self.fc.weight)
+        nn.init.xavier_uniform_(self.embedding.weight)
+    def forward(self, input,inner_states):
+        output = self.embedding(input)
+        output = self.dropout(output)
+        output,(h_n) = self.gru(output,inner_states)
+        output = self.dropout(output)
+        output = output.view(-1,200) 
+        output = self.fc(output)
+        return output,(h_n)
---
+++ ex_2/src/import_data.py
@@ -0,0 +1,36 @@
+import io
+import torch 
+
+def forbidden_list(word):
+    forbbid_list = ['<unk>','N','$']
+    return True
+    if word in forbbid_list:
+        return False
+    else:
+        return True
+
+def import_ptb_dataset(dataset_type = 'train',path = './data/ptb',batch_size = 20,vocabulary = {}):
+    dataset_path = path + '/'+ 'ptb.' + dataset_type + '.txt'
+    # Here we read each line, and append each word that has not appered previously to the list
+    with open(dataset_path) as f:
+        text = f.read()
+        # We take the text, split it (default  by sapce) than define it as a set(to remove duplicates) and finally sort it to have consistensy.
+        if len(vocabulary)<1:
+            text_splited = text[1:].split(' ')
+            word_span = sorted(set(text_splited))
+            vocabulary = {}
+            for count,word in enumerate(word_span):
+                vocabulary[word] = count
+        else:
+            text_splited = text[1:].split(' ')
+    # Here we take a large vector that maps the relative positions of the words in vocabulary
+    word_pos = []
+    for word in text_splited:
+        word_pos.append(vocabulary[word])
+    length_var = len(word_pos)
+    num_allowed = (length_var//batch_size)*batch_size
+    word_pos = torch.Tensor(word_pos[:num_allowed])
+    # Super important
+    # For some reason when using the embedding layer as input, if the input is floating rather than integer 
+    # the back prop algorithm is severely downgraded (factor of 3 w.r.t perplexity)
+    return word_pos.view(batch_size,-1).int(),vocabulary
