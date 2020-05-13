0823da3aa30296f47baff389f19d8dd6a01f9d0d
ShaharLutatiPersonal <shahar761@gmail.com>
2020-05-13 23:09:19 +0200

    bug fixes - chunking and transposed convolution as deconv

--- project/model.py
+++ project/model.py
@@ -31,96 +31,95 @@ Batch_SizexChannel_SizexWidthxLengthxSpeakers?
 
 
 class WolfModel(nn.Module):
-    def __init__(self,N,L,K,R,num_blocks):
+    def __init__(self,N,L,K,num_blocks,T):
         super(WolfModel, self).__init__()
-        self.N,self.L,self.K,self.R,self.num_blocks= N,L,K,R,num_blocks
+        self.N,self.L,self.K,self.num_blocks= N,L,K,num_blocks
+        xx,ss = self._Segmentation(torch.randn(1,N,T//2),self.K)
+        self.R = xx.shape[3]
+        R = self.R
+        print(xx.shape)
         self.num_speakers = 2
         self.encoding = nn.Sequential(nn.Conv1d(in_channels=1,out_channels=N,kernel_size=L,stride=int(L/2)),nn.ReLU())
         self.seperation_net = SeperationNet(in_channels= N,out_channels = N,hidden_channels = 128,num_layer = num_blocks,num_speakers = 2)
         self.decoding = nn.Sequential(nn.PReLU(),nn.Conv2d(in_channels=R,out_channels=self.num_speakers*R,kernel_size=1))
-        self.deconv = nn.Sequential(nn.Conv1d(in_channels=N,out_channels=1,kernel_size=L))
+        self.deconv = nn.Sequential(nn.ConvTranspose1d(in_channels=N,out_channels=1,kernel_size=L,stride=L//2,bias = False))
         # According to the paper we should use weights initialization
         # Reading Xavier weight initialization paper for symmetric non linearty (if we used relu we use Kaimen)
         # we normalize by using the factor sqrt(6)/sqrt(num_input + num_output)
         #for parameter in self.parameters():
         #    nn.init.xavier_uniform_(parameter)
 
-    def overlap_and_add(self,signal, frame_step,gap):
-        """Reconstructs a signal from a framed representation.
-        Adds potentially overlapping frames of a signal with shape
-        `[..., frames, frame_length]`, offsetting subsequent frames by `frame_step`.
-        The resulting tensor has shape `[..., output_size]` where
-            output_size = (frames - 1) * frame_step + frame_length
-        Args:
-            signal: A [..., frames, frame_length] Tensor. All dimensions may be unknown, and rank must be at least 2.
-            frame_step: An integer denoting overlap offsets. Must be less than or equal to frame_length.
-        Returns:
-            A Tensor with shape [..., output_size] containing the overlap-added frames of signal's inner-most two dimensions.
-            output_size = (frames - 1) * frame_step + frame_length
-        Based on https://github.com/tensorflow/tensorflow/blob/r1.12/tensorflow/contrib/signal/python/ops/reconstruction_ops.py
-        """
-        outer_dimensions = signal.size()[:-2]
-        frames, frame_length = signal.size()[-2:]
-        #print(outer_dimensions,frames,frame_length)
-        subframe_length = math.gcd(frame_length, frame_step)  # gcd=Greatest Common Divisor
-        subframe_step = frame_step // subframe_length
-        subframes_per_frame = frame_length // subframe_length
-        output_size = frame_step * (frames - 1) + frame_length
-        output_subframes = output_size // subframe_length
-        #print(output_subframes,frames,frame_length)
-        subframe_signal = signal.reshape(*outer_dimensions, -1, subframe_length)
-
-        frame = torch.arange(0, output_subframes).unfold(0, subframes_per_frame, subframe_step)
-        frame = signal.new_tensor(frame).long()  # signal may in GPU or CPU
-        frame = frame.contiguous().view(-1)
-
-        result = signal.new_zeros(*outer_dimensions, output_subframes, subframe_length)
-        result.index_add_(-2, frame, subframe_signal)
-        result = result.view(*outer_dimensions, -1)
+    def _over_add(self, input, gap):
+        '''
+           Merge sequence
+           input: [B, N, K, R , C]
+           gap: padding length
+           output: [B, N, L ,C] 
+        '''
+        B, N, K, R, C = input.shape
+        P = K // 2
+        # [B, N, S, K]
+        input = input.transpose(2, 3).contiguous().view(B, N, -1, K * 2,C)
+        input1 = input[:, :, :, :K,:].contiguous().view(B, N, -1,C)[:, :, P:,:]
+        input2 = input[:, :, :, K:,:].contiguous().view(B, N, -1,C)[:, :, :-P,:]
+        input = input1 + input2
+        # [B, N, L]
         if gap > 0:
-                result = result[:, :, :,:-gap]
-        return result
+            input = input[:, :, :-gap,:]
+        return input
 
-    def chunking(self,input,K):
-        B, N, T = input.shape
-        P = K // 2
-        input, gap = self.zero_padding(input,K)
-        input1 = input[:,:,:-P].contiguous()
-        input2 = input[:,:,P:].contiguous()
-        #print(input1.shape)
-        input = torch.cat([input1,input2],dim = 2).view(B,N,-1,K).transpose(2,3)
-        return input.contiguous(),gap
 
-    def zero_padding(self, input, K):
-        B, N, T = input.shape
+    def _padding(self, input, K):
+        '''
+           padding the audio times
+           K: chunks of length
+           P: hop size
+           input: [B, N, L]
+        '''
+        B, N, L = input.shape
         P = K // 2
-        gap = K - (P + T % K) % K
+        gap = K - (P + L % K) % K
         if gap > 0:
             pad = torch.Tensor(torch.zeros(B, N, gap)).type(input.type())
             input = torch.cat([input, pad], dim=2)
+
         _pad = torch.Tensor(torch.zeros(B, N, P)).type(input.type())
         input = torch.cat([_pad, input, _pad], dim=2)
-        return input, gap    
+
+        return input, gap
+    def _Segmentation(self, input, K):
+        '''
+           the segmentation stage splits
+           K: chunks of length
+           P: hop size
+           input: [B, N, L]
+           output: [B, N, K, R]
+        '''
+        B, N, L = input.shape
+        P = K // 2
+        input, gap = self._padding(input, K)
+        # [B, N, K, S]
+        input1 = input[:, :, :-P].contiguous().view(B, N, -1, K)
+        input2 = input[:, :, P:].contiguous().view(B, N, -1, K)
+        input = torch.cat([input1, input2], dim=3).view(
+            B, N, -1, K).transpose(2, 3)
+        return input.contiguous(), gap
     
     def forward(self, input):
         y = self.encoding(input)
-        #print(y.shape)
-        Chunks,gap = self.chunking(y,self.K)
+        Chunks,gap = self._Segmentation(y,self.K)
         B,N,K,R = Chunks.shape
-        #print(Chunks.shape)
+        print(Chunks.shape)
         new_outputs = [self.seperation_net.blocklist[0](Chunks)]
-        #print(new_outputs[0].shape)
         for i in range(1,self.num_blocks):
-            new_outputs.append(self.seperation_net.blocklist[i](new_outputs[i-1]))
-        a = self.decoding(new_outputs[0]).view(B,N,K,R,self.num_speakers).contiguous().permute(0,1,4,3,2)
-        #print('shape a')
-        #print(a.shape)
-        new_outputs = [self.overlap_and_add(self.decoding(x).view(B,N,K,R,self.num_speakers).contiguous().permute(0,1,4,3,2),self.K//2,gap) for x in new_outputs]
+            new_outputs.append(self.seperation_net.blocklist[i](new_outputs[i-1].contiguous().permute(0,2,3,1)))
+        new_outputs = [self._over_add(self.decoding(x).view(B,N,K,R,self.num_speakers),gap) for x in new_outputs]
         deconv_outputs = []
         for x in new_outputs:
             tmp = []
             for ix in range(self.num_speakers):
-                tmp.append(self.deconv(x[:,:,ix,:]))
+                print(x.shape)
+                tmp.append(self.deconv(x[:,:,:,ix]))
             deconv_outputs.append(tmp)
         return deconv_outputs
 
@@ -137,7 +136,7 @@ class SeperationNet(nn.Module):
 class SepBlock(nn.Module):
         def __init__(self,out_channels,hidden_channels,num_speakers):
             super(SepBlock,self).__init__()
-            #print(out_channels,hidden_channels,num_speakers)
+            ##print(out_channels,hidden_channels,num_speakers)
             self.gru_1 = nn.LSTM(input_size=out_channels,hidden_size=hidden_channels,num_layers=1,batch_first=True,bidirectional=True,dropout=0)
             self.gru_2 = nn.LSTM(input_size=out_channels,hidden_size=hidden_channels,num_layers=1,batch_first=True,bidirectional=True,dropout=0)
             self.gru_3 = nn.LSTM(input_size=out_channels,hidden_size=hidden_channels,num_layers=1,batch_first=True,bidirectional=True,dropout=0)
